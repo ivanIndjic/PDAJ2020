ae7147a17fe1d041de0b6ab3fb9fcc84d65fae76
Ivan <ivan@kali.ivan>
2020-12-05 13:03:31 +0100

    Models

--- web-app/myPdajProject/student/models.py
+++ web-app/myPdajProject/student/models.py
@@ -7,9 +7,16 @@ from django.core.validators import MaxValueValidator
 class Student(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
-    index_num = models.CharField(max_length=10)
+    index_num = models.CharField(max_length=10, unique=True)
     email = models.EmailField()
-class Grade(models.Model):
-    value = models.PositiveIntegerField(default=10, validators=[MinValueValidator(6),MaxValueValidator(10)])
-    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.index_num + ' ' + self.first_name + ' ' + self.last_name + ' ' + self.email
+    
+class Grade(models.Model):
+    value = models.PositiveIntegerField(default=10, validators=[MinValueValidator(5),MaxValueValidator(10)])
+    course = models.CharField(max_length=50)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  
+    def __str__(self):
+        return str(self.student) + ' ' + str(self.value)
